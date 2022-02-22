46e5025cae0f2a942bc544149a919acbb2648c53
Alurian-ux <74683784+Alurian-ux@users.noreply.github.com>
2022-02-22 17:14:20 +0100

    test

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 const express = require('express');
-const app = express();
-var cors = require('cors');
+const app = express()
+const router = express.Router();
+const mongoClient = require('mongodb').MongoClient
+let db;
+const url = 'mongodb+srv://ini:Kikalka1a2@cluster0.ndldf.mongodb.net/test';
+mongoClient.connect(url, (err, client) => {
+    if(err != null)
+    {
+        console.log(err.message);
+        throw err;
+    }
+    db =  client.db('MyDB')
+    console.log('connected to db')
+})
+app.listen(8000, () => {
+    console.log('running')
+})
 
-app.use(
-    cors({
-        credentials: true,
-        origin: 'https://xenodochial-gates-cf6795.netlify.app'
+router.route('/pokemon')
+    .get((req, res) => {
+        db.collection('pokemon').find(req.query).toArray(function (err, pokemon){
+            res.json(pokemon)
+        })
     })
-);
-app.options('*', cors());
+    .post((req, res) => {
+        console.log(req.query)
+        var newUser = req.query 
+        db.collection('pokemon').insert(newUser, function (err, pokemon){
+            res.json(pokemon)
+        })
+    })
 
-app.get('/', (req, res) => res.send('This api works.'));
+router.route('/pokemon/:id')
+    .get((req, res) => {
+        db.collection('pokemon').find(req.params).toArray(function (err, pokemon){
+            res.json(pokemon)
+        })
+    })
+    .put((req, res) => {
+        console.log(req.params)
+        console.log(req.body)
+        key = { '_id': ObjectId(req.params.id) }
+        db.collection('pokemon').update(key, req.body, function (err, result){
+            res.end(result)
+        })
+    })
+    .delete((req, res) => {
+        key = { '_id': ObjectId(req.params.id) }
+        db.collection('pokemon').deleteOne(key, function (err, pokemon){
+            res.json(pokemon)
+        })
+    })
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('server running on port 3000', '');
-});
+app.use('/api', router)
+
